e8aad1c631db2b782ef971f9fc7cd031ba3538a7
DONGVAN <viendong194@gmail.com>
2018-11-03 11:04:40 +0900

    add home

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    response = HttpResponse()
+    response.writelines('<h1>Hello world</h1>')
+    response.write('effort to learn django')
+    return response
+  
