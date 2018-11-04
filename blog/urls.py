2f4125d7c79ff7d8edfc3f61f291280ba549ac80
DONGVAN <viendong194@gmail.com>
2018-11-04 12:55:51 +0900

    add post

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
   path('',views.list),
-  #path('contact/',views.contact),
+  path('<int:id>/',views.post),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
+
+
 def list(request):
     Data = {"Posts": Post.objects.all().order_by("-date")}
-    return render(request,'blog/blog.html',Data)
+    return render(request, 'blog/blog.html', Data)
+
+
+def post(request, id):
+    Data_Post = {"Post": Post.objects.get(id=id)}
+    return render(request, 'blog/post.html', Data_Post)
